4d9ccc468b090db03f239ffc8178841ce1771262
Warren Lee <wsl10@sfu.ca>
2021-02-13 17:08:18 -0800

    added responsive viewbox

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
-const width = window.innerWidth;
-const height = window.innerHeight - 10;
+let width = window.innerWidth;
+let height = window.innerHeight - 10;
 
-let draw = SVG().addTo('#drawing')
-  , rect = draw.circle(100, 100).fill('#f06');
+let draw = SVG().addTo('body').size(width, height);
+
+window.addEventListener("resize", resizeDrawbox);
+
+
+drawCircle(draw);
+let rec = draw.rect(width / 3, height / 3).fill('#11aabb').move(width / 3, height / 3);
 
 // cardioid heart
+
+function resizeDrawbox() {
+	draw.viewbox(0, 0, window.innerWidth, window.innerHeight - 10);
+}
+
+function drawBackground(draw) {
+	draw.rect(width, height).fill('#cfedfc');
+}
+
+function drawCircle(draw) {
+	draw.circle(100).fill('#f06').move(50, 50);
+}
