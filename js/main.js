b27b6cb1c22a2638262e9aaa885cb19d17221602
Janet Mardjuki <mardjuki@sfu.ca>
2021-02-13 20:57:54 -0800

    Uh oh breaking system

--- js/main.js
+++ js/main.js
@@ -22,6 +22,12 @@ function drawTextBox() {
 	return circ;
 }
 
+function drawImageBox(imageLink) {
+    let image = draw.image(imageLink)
+    .move(1100, 220);
+	return image;
+}
+
 async function moveAlongPath(path, ticks, x, y, theta) {
 	for (let i = 0; i < ticks; i++) {
 		path.dmove(x, y).rotate(theta);
@@ -54,20 +60,48 @@ async function turnPage() {
 	page += 1;
 }
 
-
 async function main() {
 
 	window.addEventListener("resize", resizeDrawbox);
-	textbox = drawTextBox();
-	text = draw.text('Random Text in my Box').fill('white').font({size:24}).center(750, 550).opacity(1);
+    textbox = drawTextBox();
+    imageBox = drawImageBox('image/1.png');
+	text = draw.text('On a snowy Valentine\'s Day, \nJasper Bunny still could not think \nof a gift good enough for Lilly.\nJasper loved his wife very much. \nSo he couldn\'t give her just any old gift.').fill('white').font({size:24}).center(750, 550).opacity(1);
 	heartPath = drawHeartPath();
-	heartPath.scale(2).move(0, -100);
-	await moveAlongPath(heartPath, 90, -1, 1, 1);
-	await sleep(2000);
-	textbox.remove();
-	text.remove();
-	await moveAlongPath(heartPath, 60, 1, -3, 1.5);
-	await sleep(2000);
+    heartPath.scale(2).move(0, -100);
+    await sleep(8000);
+    text.remove();
+    text = draw.text('"Perhaps seeing what my \nneighbors are doing for Valentine\'s Day \nwill give me and idea for the perfect \npresent." he thought.').fill('white').font({size:24}).center(750, 550).opacity(1);
+    await sleep(5000);
+    text.remove();
+    textbox.remove();
+    imageBox.remove();
+    await moveAlongPath(heartPath, 90, -1, 1, 1);
+    textbox = drawTextBox();
+    text = draw.text('"He stopped at the Porcupines\' house. \n"We are each knitting a scarf for \nMother." said the seven porcupine children.').fill('white').font({size:24}).center(750, 550).opacity(1);
+    imageBox = drawImageBox('image/2.png');
+    await sleep(5000);
+    text.remove();
+    imageBox.remove();
+    imageBox = drawImageBox('image/3.png');
+    text = draw.text('"A handmade scarf did seem \nlike a good gift for Lilly,\n but Jasper couldn\'t get \nthe hang of knitting.').fill('white').font({size:24}).center(750, 550).opacity(1);
+    await sleep(5000);
+    imageBox.remove();
+    text.remove();
+    textbox.remove();
+    await sleep(1000);
+    textbox = drawTextBox();
+    text = draw.text('On his way, Jasper \nsaw a glow coming from the Frogs\' window.\n"I bet Miriam will have\na good suggestion." thought Jasper.').fill('white').font({size:24}).center(750, 550).opacity(1);
+    await sleep(5000);
+    text.remove();
+    textbox.remove();
+    await moveAlongPath(heartPath, 60, 1, -3, 1.5);
+    textbox = drawTextBox();
+    text = draw.text('Inside, Miriam showed Jasper \na box fill of chocolate-covered \nflies. "Landon will be so\nexcited when he wakes up!" she whispered.\n"I\'m sure he will." Jasper agreed pleasantly."').fill('white').font({size:24}).center(750, 550).opacity(1);
+    text.remove();
+    await sleep(2000);
+    text = draw.text('He thought, "This is a treat for a frog-but it\'s definitely not something my Lilly would enjoy!"').fill('white').font({size:24}).center(750, 550).opacity(1);
+    text.remove();
+    textbox.remove();
 	await moveAlongPath(heartPath, 90, -1, 1, 1);
 	await sleep(2000);
 	await moveAlongPath(heartPath, 30, -1, -2, 3);
